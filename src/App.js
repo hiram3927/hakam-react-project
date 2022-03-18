35d81c00117299f283c44c8cdccd0faae59e5129
나진주 <jinju@liveklass.com>
2022-03-18 20:04:20 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { AppBar, BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import { Home, Face, People } from "@material-ui/icons";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
+import HomePage from './pages/Home';
+import FeedPage from './pages/Feed';
+import AboutPage from './pages/About';
 
-function App() {
+const App = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    console.log(newValue)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="page-container">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/feed" component={FeedPage} />
+          <Route path="/about" component={AboutPage} />
+        </div>
+        <AppBar position="fixed" color="primary" style={{ top: "auto", bottom: 0 }}>
+          <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+            <BottomNavigationAction
+              component={Link}
+              to="/"
+              label="Home"
+              value="home"
+              icon={<Home />}
+            />
+            <BottomNavigationAction
+              component={Link}
+              to="/feed"
+              label="Feed"
+              value="feed"
+              icon={<People />}
+            />
+            <BottomNavigationAction
+              component={Link}
+              to="/about"
+              label="Profile"
+              value="profile"
+              icon={<Face />}
+            />
+          </BottomNavigation>
+        </AppBar>
+      </BrowserRouter>
     </div>
   );
 }
